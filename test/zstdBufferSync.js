4aff6bca634b5a4980b4a10dd489d18751712b85
XiNGRZ <hi@xingrz.me>
2021-11-02 18:15:39 +0800

    Format tests

--- test/zstdBufferSync.js
+++ test/zstdBufferSync.js
@@ -13,59 +13,59 @@ function testBufferSync(method, bufferFile, resultFile, params) {
   expect(output).to.deep.equal(result);
 }
 
-describe('ZSTD Buffer Sync', function() {
-  describe('compress', function() {
-    it('should compress binary data', function() {
+describe('ZSTD Buffer Sync', function () {
+  describe('compress', function () {
+    it('should compress binary data', function () {
       testBufferSync(zstd.compressSync, 'data10k.bin', 'data10k.bin.zst');
     });
 
-    it('should compress text data', function() {
+    it('should compress text data', function () {
       testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst');
     });
 
-    it('should compress text data with level=3', function() {
-      testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst.03', {level: 3});
+    it('should compress text data with level=3', function () {
+      testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst.03', { level: 3 });
     });
 
-    it('should compress text data with level=9', function() {
-      testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst.09', {level: 9});
+    it('should compress text data with level=9', function () {
+      testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst.09', { level: 9 });
     });
 
-    it('should compress text data with dict=(new Buffer("hello zstd"))', function() {
-      testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst.09.dict', {level: 9, dict: Buffer.from("hello zstd")});
+    it('should compress text data with dict=(new Buffer("hello zstd"))', function () {
+      testBufferSync(zstd.compressSync, 'data.txt', 'data.txt.zst.09.dict', { level: 9, dict: Buffer.from("hello zstd") });
     });
 
-    it('should compress an empty buffer', function() {
+    it('should compress an empty buffer', function () {
       testBufferSync(zstd.compressSync, 'empty', 'empty.zst');
     });
 
-    it('should compress a large buffer', function() {
+    it('should compress a large buffer', function () {
       testBufferSync(zstd.compressSync, 'large.txt', 'large.txt.zst');
     });
   });
 
-  describe('decompress', function() {
-    it('should decompress binary data', function() {
+  describe('decompress', function () {
+    it('should decompress binary data', function () {
       testBufferSync(zstd.decompressSync, 'data10k.bin.zst', 'data10k.bin');
     });
 
-    it('should decompress text data', function() {
+    it('should decompress text data', function () {
       testBufferSync(zstd.decompressSync, 'data.txt.zst', 'data.txt');
     });
 
-    it('should decompress text data with dict=(new Buffer("hello zstd"))', function() {
-      testBufferSync(zstd.decompressSync, 'data.txt.zst.09.dict', 'data.txt', {level: 9, dict: Buffer.from("hello zstd")});
+    it('should decompress text data with dict=(new Buffer("hello zstd"))', function () {
+      testBufferSync(zstd.decompressSync, 'data.txt.zst.09.dict', 'data.txt', { level: 9, dict: Buffer.from("hello zstd") });
     });
 
-    it('should decompress to an empty buffer', function() {
+    it('should decompress to an empty buffer', function () {
       testBufferSync(zstd.decompressSync, 'empty.zst', 'empty');
     });
 
-    it('should decompress to a large buffer', function() {
+    it('should decompress to a large buffer', function () {
       testBufferSync(zstd.decompressSync, 'large.zst', 'large');
     });
 
-    it('should decompress to another large buffer', function() {
+    it('should decompress to another large buffer', function () {
       testBufferSync(zstd.decompressSync, 'large.txt.zst', 'large.txt');
     });
   });
